5c12bfc3c95ee0d48511acab9cab02ba5f1f47fa
Corentinrmt <corentin.remot@gmail.com>
2025-01-28 16:18:37 +0100

    [UPDATE] transition screen juici

--- Assets/_project/scripts/UI/DaysTransitions/UIDaysTransitions.cs
+++ Assets/_project/scripts/UI/DaysTransitions/UIDaysTransitions.cs
@@ -10,14 +10,19 @@ namespace NarrativeProject
     {
         #region Fields
         [SerializeField] private Transform _containerTransition;
+        [SerializeField] private Transform _btnNewsPaper;
         [SerializeField] private Transform _show;
         [SerializeField] private Transform _hide;
 
         [SerializeField] private float _duration;
 
+        [SerializeField] private float _newsPaperAppearDuration;
+
         [SerializeField] private Ease _ease;
 
         private Tween _transitionTween;
+        private Tween _transitionNewsPaperTween;
+        private Tween _transitionRotNewsPaperTween;
 
         #endregion
 
@@ -38,7 +43,7 @@ namespace NarrativeProject
             {
                 _transitionTween.Kill();
             }
-            _containerTransition.DOMove(_show.position, _duration).SetEase(_ease);
+            _transitionTween = _containerTransition.DOMove(_show.position, _duration).SetEase(_ease).OnComplete(() => ShowNewsPaper());
         }
 
         [Button]
@@ -48,7 +53,39 @@ namespace NarrativeProject
             {
                 _transitionTween.Kill();
             }
-            _containerTransition.DOMove(_hide.position, _duration);
+            _transitionTween = _containerTransition.DOMove(_hide.position, _duration).OnComplete(() => HideNewsPaper());
         }
+
+        public void ShowNewsPaper()
+        {
+            if (_transitionNewsPaperTween != null)
+            {
+                _transitionNewsPaperTween.Kill(true);
+            }
+            if (_transitionRotNewsPaperTween != null)
+            {
+                _transitionRotNewsPaperTween.Kill(true);
+            }
+
+            _btnNewsPaper.transform.rotation = Quaternion.identity;
+            _btnNewsPaper.transform.localScale = Vector3.zero;
+
+            _btnNewsPaper.gameObject.SetActive(true);
+            _transitionNewsPaperTween = _btnNewsPaper.DOScale(1f, _newsPaperAppearDuration);
+            _transitionRotNewsPaperTween = _btnNewsPaper.DORotate(new Vector3(0f, 0f, 380f), 0.7f, RotateMode.FastBeyond360);
+        }
+        public void HideNewsPaper()
+        {
+            if (_transitionNewsPaperTween != null)
+            {
+                _transitionNewsPaperTween.Kill(true);
+            }
+            if (_transitionRotNewsPaperTween != null)
+            {
+                _transitionRotNewsPaperTween.Kill(true);
+            }
+            _transitionNewsPaperTween = _btnNewsPaper.transform.DOScale(Vector3.zero, _newsPaperAppearDuration);
+        }
+
     }
 }
