760de5bb188599b7bc00fc3b79a3b6bb44174fc9
Corentinrmt <corentin.remot@gmail.com>
2025-02-06 14:54:02 +0100

    [FIX] build + reduce resolution anim to reduce package size

--- Assets/_project/scripts/DialogDebug/DialogInventoryToGameLink.cs
+++ Assets/_project/scripts/DialogDebug/DialogInventoryToGameLink.cs
@@ -2,7 +2,6 @@ using CREMOT.DialogSystem;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Plastic.Newtonsoft.Json.Bson;
 using UnityEngine;
 
 namespace NarrativeProject
