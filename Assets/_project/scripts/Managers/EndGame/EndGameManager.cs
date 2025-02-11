7b22b3584333941fe2257d3dc7edadec6bbbfbbe
CorentinRmt <corentin.remot@gmail.com>
2025-02-11 14:43:22 +0100

    [UPDATE] setup endgame so that it can easily be call trough graph

--- Assets/_project/scripts/Managers/EndGame/EndGameManager.cs
+++ Assets/_project/scripts/Managers/EndGame/EndGameManager.cs
@@ -9,6 +9,8 @@ namespace NarrativeProject
 {
     public class EndGameManager : MonoBehaviour
     {
+        [SerializeField] private Canvas _canvasEndGame;
+
         string title;
 
         string fact_1 = "";
@@ -20,6 +22,42 @@ namespace NarrativeProject
         [SerializeField] Image imgSprite;
         public UnityEvent OnOpenEndGameUnity;
 
+        private void Start()
+        {
+            if (_canvasEndGame)
+                _canvasEndGame.gameObject.SetActive(false);
+        }
+
+        public void SetEndingTitle(int endingIndex)
+        {
+            switch (endingIndex)
+            {
+                case 0: // Axeman
+                    title = "BREAKING NEWS: FARM WORKER NAMED ALBERT AXEMAN FOUND DEAD IN GARBAGE CAN OF ILLEGAL BAR!";
+                    break;
+                case 1: // Log Lady
+                    title = "BREAKING NEWS: DIVA NAMED LINDA LOG-LADY FOUND DEAD IN GARBAGE CAN OF ILLEGAL BAR!";
+                    break;
+                case 2: // Lenny
+                    title = "BREAKING NEWS: THE CRAZY MAN WITH A FORK NAMED LENNY THE DEVIL FOUND DEAD IN THE TRASH OF AN ILLEGAL BAR!";
+                    break;
+                case 3: // Phil
+                    title = "BREAKING NEWS: DANDY NAMED BEAUTIFUL PHILIBERT FOUND DEAD IN THE TRASH OF AN ILLEGAL BAR!";
+                    break;
+                case 4: // Crackhead
+                    title = "BREAKING NEWS: POLICE OFFICER NAMED SERGEANT CRACKHEAD FOUND DEAD IN GARBAGE CAN OF ILLEGAL BAR!";
+                    break;
+                case 5: // Muddy
+                    title = "BREAKING NEWS: MURDER OF A HOMELESS MAN, A NEW KILLER IN CITY";
+                    break;
+                case 6: // Kina
+                    title = "BREAKING NEWS: A SECOND SUICIDE HAS TAKEN PLACE IN DOWNTOWN, AN EPIDEMIC OF DEPRESSION OR A SIMPLE COINCIDENCE?";
+                    break;
+                case 7: // The player
+                    title = "BREAKING NEWS: A SECOND SUICIDE HAS TAKEN PLACE IN DOWNTOWN, AN EPIDEMIC OF DEPRESSION OR A SIMPLE COINCIDENCE?";
+                    break;
+            }
+        }
 
         public void NewFact(string newFact)
         {
@@ -39,10 +77,13 @@ namespace NarrativeProject
 
         public void OpenEndGame()
         {
-            OnOpenEndGameUnity?.Invoke();
+            if (_canvasEndGame)
+                _canvasEndGame.gameObject.SetActive(true);
+
             titleText.text = title;
             fact1.text = fact_1;
             fact2.text = fact_2;
+            OnOpenEndGameUnity?.Invoke();
         }
     }
 }
